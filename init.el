caf3da2ed87580a3e402e954cceb70a22b9f1412
Matthew Westrik <m@ttwestrik.com>
2018-08-17 01:35:22 -0400

    Install solarized

--- init.el
+++ init.el
@@ -7,6 +7,15 @@
 
 (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+(package-install 'use-package))
+
+(use-package solarized-theme
+  :ensure t
+  :init
+(load-theme 'solarized-dark))
+
 ;; Fullscreen
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
@@ -21,4 +30,18 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (tooltip-mode -1)
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" default)))
+ '(package-selected-packages (quote (apropospriate-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

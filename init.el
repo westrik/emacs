8e26a2a0e89aaf84f2ffdf2f9cf4f32a74510feb
Matthew Westrik <m@ttwestrik.com>
2018-08-27 01:00:01 -0400

    use pragmatapro

--- init.el
+++ init.el
@@ -57,3 +57,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+(add-to-list 'default-frame-alist '(font . "Pragmata Pro Mono" ))
+(set-face-attribute 'default t :font "Pragmata Pro Mono-16")
